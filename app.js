8cfe1334b14899148a29488e98d57d8e91ddb03a
SudiptoRahaOfficial <sudiptoraha.official@gmail.com>
2026-06-14 17:00:11 +0600

    maked clutter folder path and clutter folder scaning feature

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // importing fs/promises nodejs module
 const fs = require('node:fs/promises')
+// importing path nodejs module
+const path = require('node:path')
+
+// making files directory path
+const filesDir = path.join(__dirname, 'clutters')
+
+;(async function () {
+	// scaning files directory
+	const files = await fs.readdir(filesDir)
+
+    // traversing all files inside clutters folder
+    for (const file of files) {
+        
+    }
+})()
